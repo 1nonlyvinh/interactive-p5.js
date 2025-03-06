d8d67256664db85eecec94d3982c69f0c08bc497
1nonlyvinh <vincenth2898@tuckahoeschools.org>
2025-03-06 13:29:08 +0000

    start

---
+++ sketch.js
@@ -0,0 +1,13 @@
+let sunHeight;
+let horizon = 200;
+
+function setup() {
+    createCanvas(1600, 800);
+    background(255, 201, 34);  
+}
+
+function draw() {
+    circle(800, 200, 200);
+    fill(255, 100, 0, 100);
+
+}
